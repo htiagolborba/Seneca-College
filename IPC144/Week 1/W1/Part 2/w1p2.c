c53d9d3008d484981f07e139d105aac4f6c27548
htiagolborba <107688645+htiagolborba@users.noreply.github.com>
2023-05-27 20:46:40 -0400

    Add files via upload

---
+++ IPC144/Week 1/W1/Part 2/w1p2.c
@@ -0,0 +1,52 @@
+/*
+                              Reflection
+                             Workshop - #1
+Full Name  : HIRAN TIAGO LINS BORBA
+Student ID#:  147216220
+Email      : htlins-borba@myseneca.ca
+Section    : IPC144
+
+Authenticity Declaration:
+
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+
+
+#include <stdio.h>
+
+int main() {
+    printf("Workshop 1 Part-1\n");
+    printf("=================\n\n");
+    printf("I'm displaying this using the 'printf' stdio\n");
+    printf("(standard input output) library function!\n\n");
+    printf("Dear teacher,\n\n");
+    printf("  I promise I will work hard from this day onward.\n");
+    printf("  I acknowledge that practice is extremely important,\n");
+    printf("  so I will do all workshops, quizzes, and assignments.\n\n");
+    printf("Sincerely,\n\n");
+    printf("Hiran Borba\n");
+    printf("Student ID#: 147216220");
+
+    return 0;
+}  
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Workshop 1 Part-2\n");
+    printf("=================\n");
+    printf("Using tab specifiers...\n");
+    printf("\t Tab-1\tTab-2\tTab-3 \n \t-----\t=====\t-----\n");
+    printf("\\ this is a back-slash character!\n");
+    printf("%% this is a percent sign character!\n");
+    printf("\" this is a double-quote character!\n");
+    printf("My favourite quotes are:\n");
+    printf("1.\t \"Skill is only developed by hours and hours of work.\" \n\t\t\t\t\t\t -Usain Bolt\n");
+    printf("2.\t \"It's not about having time. It's about making time.\" \n\t\t\t\t\t\t -unknown\n");
+    printf("3.\t \"All of us do not have equal talent. But, all of us\n");
+    printf("\t have an equal opportunity to develop our talents.\" \n\t\t\t\t\t\t -A.P.J. Abdul Kalam\n");
+
+    return 0;
+}
