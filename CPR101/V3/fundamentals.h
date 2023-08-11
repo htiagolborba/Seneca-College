06471af109d48a046338b144adc3f10fdad88940
¨Hiran <htiago-logins@hotmail.com>
2023-08-11 12:01:56 -0400

    Versin 2 CPR101 Final project

---
+++ CPR101/V3/fundamentals.h
@@ -0,0 +1,13 @@
+                                                        /* fundamentals module header version 1 */
+
+
+#ifndef _FUNDAMENTALS_H_        // If the "fundamentals.h" file has been executed previously, it will not run again, as the "ifndef" (if not defined) check prevents it from being included multiple times.
+#define _FUNDAMENTALS_H_	    // define _FUNDAMENTALS_H_ as to prevent reruns of the header file
+
+#include <stdio.h>			    // include standard input/output lib
+#include <string.h>			    // include the string manipulation lib
+#include <stdlib.h>			    // include the standard library functions lib
+
+void fundamentals(void);	    // prototype function declared here
+
+#endif						    // end conditional definitions
